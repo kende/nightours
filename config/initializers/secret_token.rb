643aacff59fcf59b3a08b010048aa9f5ce23dbbe
kende <kende.j@gmail.com>
2011-11-02 00:57:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nightours::Application.config.secret_token = '58ee9cbbb4d9eda99b541135eb9e6bdea96b34397e9a22f1d15165b28ee92d941325b4dd8cd5a7e8d66e52add033dde54a3f4f376aa65a7d2f6800f4800b208c'
